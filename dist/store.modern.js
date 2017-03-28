46e37cb4aaaf55eb0dddc57b5ebbdc87d27574c6
monove <overlander@gmail.com>
2017-03-28 11:34:34 -0400

    Remove localStorage

--- dist/store.modern.js
+++ dist/store.modern.js
@@ -1,7 +1,6 @@
 var engine = require('../src/store-engine')
 
 var storages = [
-	require('../storages/localStorage'), 
 	require('../storages/sessionStorage'), 
 	require('../storages/cookieStorage'), 
 	require('../storages/memoryStorage'),
